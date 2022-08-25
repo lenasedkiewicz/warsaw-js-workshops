167f8635494e6b872c2814f360cbaffa6a18e2cc
wokayme <kontakt@seapage.pl>
2022-08-25 20:35:48 +0200

    Prepare basic application layout

---
+++ navigation.js
@@ -0,0 +1,20 @@
+const navigationLinks = document.querySelectorAll('li[data-page]');
+const allPages = document.querySelectorAll('.page');
+
+navigationLinks.forEach(link=>{
+    link.addEventListener('click', (e)=>{
+        allPages.forEach(page=>{
+            page.style.display = "none";
+        })
+        const pageActive = e.target.dataset.page;
+        document.querySelector(`#${pageActive}`).style.display = "block";
+    })
+})
+
+//hide all pages except first
+allPages.forEach((page, index)=>{
+    if(index===0){
+        return;
+    }
+    page.style.display = "none";
+})
