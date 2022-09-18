87b736c354d477e7e7abebb0e77cbef5c067cda9
Magdalena Sędkiewicz <magdalenainspirations@gmail.com>
2022-09-18 11:59:41 +0200

    switch off console log

--- nbp-data.js
+++ nbp-data.js
@@ -2,7 +2,7 @@ fetch('http://api.nbp.pl/api/exchangerates/tables/A/?format=json').then(function
     return res.json()
 }).then(function(r){
     const rates = r[0].rates;
-    console.log(rates);
+    // console.log(rates);
     for (rate of rates) {
         appendRow("exchange_rate", [rate.currency, rate.mid + ` zł`])
     }
