6ac23ac713d1e7e0ae5b0628c03fb1f0edd18f63
Magdalena Sędkiewicz <magdalenainspirations@gmail.com>
2022-09-18 14:21:51 +0200

    Add local storage cookie

--- navigation.js
+++ navigation.js
@@ -7,6 +7,15 @@ navigationLinks.forEach(link=>{
             page.style.display = "none";
         })
         const pageActive = e.target.dataset.page;
+        console.log(pageActive);
+        localStorage.setItem('pageActive', pageActive)
+        // if (pageActive.includes(track-saving)) {
+        //     localStorage.setItem('pageActive', 'track-saving')
+        // } else if (pageActive.includes(investor-calculator)) {
+        //     localStorage.setItem('pageActive', 'investor-calculator')
+        // } else if (pageActive.includes(exchange-rates)) {
+        //     localStorage.setItem('pageActive', 'exchange-rates')
+        // }
         document.querySelector(`#${pageActive}`).style.display = "block";
     })
 })
