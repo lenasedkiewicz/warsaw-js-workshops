91d1104213593bd5c4261334bbe4d2bc5740ecb0
Magdalena Sędkiewicz <magdalenainspirations@gmail.com>
2022-09-18 11:39:10 +0200

    Add rows with data

--- nbp-data.js
+++ nbp-data.js
@@ -1,5 +1,9 @@
 fetch('http://api.nbp.pl/api/exchangerates/tables/A/?format=json').then(function(res){
     return res.json()
 }).then(function(r){
-    console.log(r)
+    const rates = r[0].rates;
+    console.log(rates);
+    for (rate of rates) {
+        appendRow("exchange_rate", [rate.currency, rate.mid + ` zł`])
+    }
 })
